84b0d823f098b352ba20d3ce6df75950b168a700
Zia Ahmed <ziaahmedf17@gmail.com>
2025-05-11 22:06:11 +0500

    Checkout Screen Done
    Checkout Screen

---
+++ JuiceCorner/CheckoutView.swift
@@ -0,0 +1,38 @@
+//
+//  CheckoutView.swift
+//  JuiceCorner
+//
+//  Created by Zi on 11/05/2025.
+//
+
+import SwiftUI
+
+struct CheckoutView: View {
+    @ObservedObject var order: Order
+    
+    var body: some View {
+        ScrollView{
+            VStack{
+                AsyncImage(url: URL(string: "https://unsplash.com/photos/orange-juice-in-clear-drinking-glass-TmOGarNOGFs"), scale: 3){image in
+                        image
+                        .image?.resizable()
+                        .scaledToFit()
+                } placeholder: {
+                    ProgressView()
+                }
+                .frame(height: 233)
+                
+                Text("Your cost is \(order.cost, format: .currency(code: "USD"))")
+                    .font(.title)
+                Button("Place Order", action: {})
+                    .padding()
+            }
+        }
+        .navigationTitle("Checkout")
+        .navigationBarTitleDisplayMode(.inline)
+    }
+}
+
+#Preview {
+    CheckoutView(order: Order())
+}
--- JuiceCorner/Order.swift
+++ JuiceCorner/Order.swift
@@ -25,5 +25,35 @@ class Order: ObservableObject
     @Published var extraFrosting = false
     @Published var addSprinkles = false
     
+    @Published var name = ""
+    @Published var streetAddress = ""
+    @Published var city = ""
+    @Published var zip = ""
     
+    var hasValidAddress: Bool{
+        if name.isEmpty || streetAddress.isEmpty || city.isEmpty || zip.isEmpty{
+            return false
+        }
+        return true
+    }
+    
+    var cost: Double{
+        
+        //$2 per juice
+        var cost = Double(quantity) * 2.5
+        
+        //Customized juice cost more
+        cost += (Double(type) / 2.5)
+        
+        //1$ per juice for extra frosting
+        if extraFrosting{
+            cost += Double(quantity)
+        }
+        
+        //$0,50 for extra sprinkles
+        if addSprinkles{
+            cost += Double(quantity)/2.5
+        }
+        return cost
+    }
 }
