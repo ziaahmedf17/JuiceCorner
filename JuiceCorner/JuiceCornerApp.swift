4023347dc80f3c5e9eb5b4dba883411f86893546
Zia Ahmed <ziaahmedf17@gmail.com>
2025-05-11 15:56:30 +0500

    Initial Commit

---
+++ JuiceCorner/JuiceCornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  JuiceCornerApp.swift
+//  JuiceCorner
+//
+//  Created by Zi on 11/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct JuiceCornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ JuiceCornerTests/JuiceCornerTests.swift
@@ -0,0 +1,17 @@
+//
+//  JuiceCornerTests.swift
+//  JuiceCornerTests
+//
+//  Created by Zi on 11/05/2025.
+//
+
+import Testing
+@testable import JuiceCorner
+
+struct JuiceCornerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
